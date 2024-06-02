93e21a3ba42e00eba42b2f08cf188826252482bf
zainabfatima04 <144207551+zainabfatima04@users.noreply.github.com>
2024-06-02 12:19:45 +0400

    Burberry Website

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+//This function runs whenever any elements with the class 'hidden' is in the viewport
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry)
+    //Checks if the element is intersecting with the viewport
     if (entry.isIntersecting) {
+      //If the element is intersecting, it adds the 'show' class to the element
       entry.target.classList.add('show');
     } else {
+      //If the element is not intersecting, it removes the 'show' class from the element
       entry.target.classList.remove('show');
     }
   });
 });
 
 
+//Selects all elements with the class 'hidden' and observes them using the observer
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
